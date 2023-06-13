3493d7ef6bf466fdaf7b9a7a1a5563d1b65eb8ef
fightingsou <hirawatasou@gmail.com>
2023-06-14 02:17:40 +0900

    :recycle: Initialize Nuxt.js application
    - Removed all existing Vue.js files
    - Created a new Nuxt.js application

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style>
+.v-application--wrap {
+  background-color: #eee;
+  padding-top: 0;
+  min-height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="index">
+    <img src="~assets/images/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style lang="scss" scoped>
+.index {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  img {
+    width: 100%;
+    max-width: 500px;
+  }
+}
+</style>
