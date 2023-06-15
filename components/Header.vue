c4839a9236af0860bd97bc42d90b614ac6fcd7d8
fightingsou <hirawatasou@gmail.com>
2023-06-16 00:02:28 +0900

    :gem: Update project dependencies and redesign landing page
    - Added @fortawesome/fontawesome-free version 6.4.0 to the project dependencies in package.json and package-lock.json
    - Removed the old images logo.PNG and portfolio.png from the assets directory
    - Upgraded buffer package version to 6.0.3 in package-lock.json
    - Modified pages/index.vue by replacing the old logo image with a new SVG logo, and adding a new design to the main section with animation
    - Improved CSS styling for the new design in pages/index.vue

---
+++ components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="header">
+    <nav class="nav">
+      <ul class="nav__list">
+        <li class="nav__item">
+          <a class="nav__link" href="/">Home</a>
+        </li>
+        <li class="nav__item">
+          <a class="nav__link" href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+export default ({
+  name: 'Header',
+});
+</script>
+
+<style lang="scss" scoped>
+.header {
+  padding: 1rem;
+}
+
+.nav__list {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.nav__item {
+  margin-right: 1rem;
+}
+
+.nav__link {
+  color: #555;
+  font-family: sans-serif;
+  text-decoration: none;
+  transition: color 0.3s;
+}
+
+.nav__link:hover {
+  color: #ccc;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="index">
-    <img src="~assets/images/logo.png" />
-  </div>
+  <section class="index">
+    <div class="main">
+      <img class="logo" src="~assets/images/logo.svg" alt="logo" />
+      <h1 class="border"></h1>
+      <div class="scroll-icon">
+        <img class="scroll-img" src="~assets/images/arrow.svg" alt="scroll-down" />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
 }
 </script>
 
@@ -16,9 +22,48 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  img {
+  height: 90vh;
+  .logo {
+    display: block;
+    filter: drop-shadow(0 0 0.25rem #333);
     width: 100%;
     max-width: 500px;
+    margin: auto;
+    margin-right: 700px;
+  }
+  .border {
+    border-bottom: 0.5px solid #333;
+    filter: drop-shadow(0 0 0.25rem #000);
+    width: 20%;
+    max-width: 500px;
+    margin: auto;
+    margin-top: 40px;
+    margin-right: 700px;
+  }
+  .scroll-icon {
+    position: absolute;
+    color: #333;
+    bottom: 40px;
+    left: 50%;
+    font-size: 0.6em;
+    animation: bounce 2s infinite;
+    .scroll-img {
+      width: 30px;
+      height: 30px;
+      rotate: 90deg;
+    }
+  }
+
+  @keyframes bounce {
+    0%, 20%, 50%, 80%, 100% {
+      transform: translateY(0);
+    }
+    40% {
+      transform: translateY(-30px);
+    }
+    60% {
+      transform: translateY(-15px);
+    }
   }
 }
 </style>
