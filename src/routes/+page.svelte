27345b82eee57b359fd67302c0d76f8877e96f72
fightingsou <hirawatasou@gmail.com>
2023-06-17 04:54:02 +0900

    :art: Redesign SvelteKit's landing page
    Details
    - Added a Header component to the page
    - Included a logo with corresponding styles
    - Implemented a new flex layout with an underline feature and custom drop-shadow styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Header from "../components/Header.svelte";
+  const logoPath: string = "/logo.svg"
+</script>
+
+<main>
+  <Header />
+  <img src={logoPath} class="logo" alt="logo">
+  <div class="underline"></div>
+</main>
+
+<style lang="scss">
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 90vh;
+    .logo {
+      width: 30%;
+      pointer-events: none;
+      filter: drop-shadow(0 0 0.75rem #333);
+      margin-bottom: 2rem;
+    }
+    .underline {
+      width: 10%;
+      height: 1px;
+      background-color: #333;
+      filter: drop-shadow(0 0 0.75rem #333);
+    }
+  }
+</style>
+
